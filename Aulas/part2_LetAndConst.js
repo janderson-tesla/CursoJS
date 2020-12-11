ce83596eddd03081ae3d237498a0735efcc67a5f
janderson-code <jbgfilebarbosa@gmail.com>
2020-12-11 12:56:01 -0300

    Aula let e const

---
+++ Aulas/part2_LetAndConst.js
@@ -0,0 +1,32 @@
+// function sayHi(){
+
+// var shouldSayHi = true;
+// var reallySayHi = true;
+
+// if(shouldSayHi === true){
+//     var myName = "janderson";
+
+//     if(reallySayHi === true){
+//       var myLastName = "Barbosa";
+//     }
+//   }
+// console.log("Olá" + " "+ myName + " " +myLastName);
+// }
+
+// sayHi();
+/*--------------------------------------------*/
+function sayHi() {
+    let shouldSayHi = true;
+    let reallySayHi = true;
+    let myName, lastName;
+
+    if (shouldSayHi === true) {
+        myName = "Janderson";
+        console.log("Hi" + " " + myName);
+    } else {
+        myName = "Gustavo";
+        console.log("Hi" + " " + myName);
+    }
+
+}
+sayHi();
