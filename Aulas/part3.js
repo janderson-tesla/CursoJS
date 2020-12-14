27005e72c42e64efbd639a98434b16c2beb2365b
janderson-tesla <jandersonprofissional2000@gmail.com>
2020-12-14 19:34:06 -0300

    update
    Atualização aula part 3 aula 2

--- Aulas/part3.js
+++ Aulas/part3.js
@@ -1,5 +1,5 @@
 //-----------------------------------
-// IF STATEMENT
+// IF STATEMENT AULA 01
 //-----------------------------------
 // TELL THA CODE : IF THIS THING IS TRUE, RUN MORE CODE
 // IF(EXPRESSION)
@@ -12,5 +12,22 @@ if (shouldSayHi== true) {
 	console.log("Hi there")
 }
 //-----------------------------------
-// LECTURE : COMPARISON OPERATORS
+// LECTURE : COMPARISON OPERATORS AULA 02
 //-----------------------------------
+
+const numberFive = 5;// se for 4 dá false
+console.log(typeof numberFive); // number
+
+const stringFive = "5";
+console.log(typeof stringFive); //string
+
+console.log(numberFive === stringFive);// false na primeira vez de debug , na segunda é true
+console.log(numberFive === stringFive); // true
+console.log(numberFive + stringFive); // 55
+
+
+
+
+
+
+
