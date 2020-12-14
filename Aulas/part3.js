392fe5b74888063d6ad53d8140e7e6d9dde348ea
janderson-tesla <jandersonprofissional2000@gmail.com>
2020-12-14 18:37:14 -0300

    CREATE PART3.JS
    Aula 03 If Statements and Operators

---
+++ Aulas/part3.js
@@ -0,0 +1,16 @@
+//-----------------------------------
+// IF STATEMENT
+//-----------------------------------
+// TELL THA CODE : IF THIS THING IS TRUE, RUN MORE CODE
+// IF(EXPRESSION)
+
+
+// IF
+const shouldSayHi = true;
+
+if (shouldSayHi== true) {
+	console.log("Hi there")
+}
+//-----------------------------------
+// LECTURE : COMPARISON OPERATORS
+//-----------------------------------
