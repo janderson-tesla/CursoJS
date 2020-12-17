a9715f8d4fc564100c2bc36171fef54fdb38d58d
janderson-tesla <jandersonprofissional2000@gmail.com>
2020-12-17 19:38:49 -0300

    Update par3.js
    Atualização aula

--- Aulas/part3.js
+++ Aulas/part3.js
@@ -15,24 +15,25 @@ if (shouldSayHi== true) {
 // LECTURE : COMPARISON OPERATORS AULA 02
 //-----------------------------------
 
-/*const numberFive = 5;// se for 4 dá false
-//console.log(typeof numberFive); // number
+			/*Variables */
+const numberFive = 5;// se for 4 dá false
+console.log(typeof numberFive); // number
 
-//console.log(typeof stringFive); //string
+console.log(typeof stringFive); //string
 
-//console.log(numberFive === stringFive);//false
-//console.log(numberFive === stringFive); // true
-//console.log(numberFive + stringFive); // 55
+console.log(numberFive === stringFive);//false
+console.log(numberFive === stringFive); // true
+console.log(numberFive + stringFive); // 55
 
-//const stringConversion = parseInt(stringFive);
-//console.log(numberFive + stringConversion);// 10
+const stringConversion = parseInt(stringFive);
+console.log(numberFive + stringConversion);// 10
 
 if (numberFive === stringFive) {
 	console.log("Ola"); // imprime olá
-}*/
+}
 
 
-/*Arrays*/
+			/*Arrays*/
 
 const myArray =[1,2,3];
 const myOtherArray = [1,2,3];
@@ -40,10 +41,25 @@ const myOtherArray = [1,2,3];
 console.log(myArray === myOtherArray); // false
 console.log(myArray == myOtherArray); // false
 
+			/*Objects*/
 
+const myObject = {name:"Janderson"};
+const myOtherObject = {name : "Bob"};
 
+console.log(myObject === myOtherObject); //false
 
+			/*Operators*/
 
+const bank = 123;
+const item = 123;
+const tax= 0.50;
 
+if (bank >= item) {
+	console.log("You can purchase this item");
+
+}
+if (bank <= item) {
+	console.log("You cannot purchase this item");
+}
 
 
