7471e832edc073f594eea58b17dab1291326cbbf
janderson-tesla <jandersonprofissional2000@gmail.com>
2020-12-17 18:42:57 -0300

    Update part3.js
    Aula 3 atualizada.

--- Aulas/part3.js
+++ Aulas/part3.js
@@ -15,15 +15,31 @@ if (shouldSayHi== true) {
 // LECTURE : COMPARISON OPERATORS AULA 02
 //-----------------------------------
 
-const numberFive = 5;// se for 4 dá false
-console.log(typeof numberFive); // number
+/*const numberFive = 5;// se for 4 dá false
+//console.log(typeof numberFive); // number
 
-const stringFive = "5";
-console.log(typeof stringFive); //string
+//console.log(typeof stringFive); //string
 
-console.log(numberFive === stringFive);// false na primeira vez de debug , na segunda é true
-console.log(numberFive === stringFive); // true
-console.log(numberFive + stringFive); // 55
+//console.log(numberFive === stringFive);//false
+//console.log(numberFive === stringFive); // true
+//console.log(numberFive + stringFive); // 55
+
+//const stringConversion = parseInt(stringFive);
+//console.log(numberFive + stringConversion);// 10
+
+if (numberFive === stringFive) {
+	console.log("Ola"); // imprime olá
+}*/
+
+
+/*Arrays*/
+
+const myArray =[1,2,3];
+const myOtherArray = [1,2,3];
+
+console.log(myArray === myOtherArray); // false
+console.log(myArray == myOtherArray); // false
+
 
 
 
