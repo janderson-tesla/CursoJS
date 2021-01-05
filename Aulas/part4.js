c0326cc614ec98818bb0ccfdf60f893e9ccd8b84
janderson-tesla <jandersonprofissional2000@gmail.com>
2021-01-04 21:35:15 -0300

    Update part4.js
    Update

--- Aulas/part4.js
+++ Aulas/part4.js
@@ -1,6 +1,37 @@
 
 //Lecture: Introduction to Functions
+
 //Functions
+
+//Declarando Função
+
+const MyName ="Janderson";
+
+function sayHi(username, lastname){
+	console.log("HI" + " " + MyName + " " +lastname);
+}
+
+function sayBye(firstname,age){
+	console.log("Bye" + " " + firstname + "you are" + " " +age);
+}
+//chamando função
+sayHi("Janderson","Barbosa");
+sayHi("josé","Joaquino");
+sayHi("Maria","Madalena");
+
+
+const billy = "Billy";
+const Janderson = "Janderson";
+const sally = "sally";
+
+
+sayBye(billy,20);
+sayBye(Janderson,25);
+sayBye(sally,10);
+
+
+
+
 
 
 
