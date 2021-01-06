df29855c58784f464d8d703026efe7cd8bdb38b6
janderson-tesla <jandersonprofissional2000@gmail.com>
2021-01-06 13:38:28 -0300

    Update part4.js
    Comentando linhas anteriores, até 29:28 do primeiro video function

--- Aulas/part4.js
+++ Aulas/part4.js
@@ -1,56 +1,83 @@
 
+// aula 04 
+
 //Lecture: Introduction to Functions
 
 //Functions
 
 //Declarando Função
 
-const MyName ="Janderson";
+// const MyName ="Janderson";
 
-function sayHi(username, lastname){
-	console.log("HI" + " " + MyName + " " +lastname);
-}
-// não precisa tipar os parametros com C++ string firstname , int age
-function sayBye(firstname,age){
-	console.log("Bye" + " " + firstname + "you are" + " " +age);
-}
-//chamando função
-sayHi("Janderson","Barbosa");
-sayHi("josé","Joaquino");
-sayHi("Maria","Madalena");
-
-
-const billy = "Billy";
-const Janderson = "Janderson";
-const sally = "sally";
+// function sayHi(username, lastname){
+// 	console.log("HI" + " " + MyName + " " +lastname);
+// }
+// // não precisa tipar os parametros com C++ string firstname , int age
+// function sayBye(firstname,age){
+// 	console.log("Bye" + " " + firstname + "you are" + " " +age);
+// }
+// //chamando função
+// sayHi("Janderson","Barbosa");
+// sayHi("josé","Joaquino");
+// sayHi("Maria","Madalena");
 
 
-sayBye(billy,20);
-sayBye(Janderson,25);
-sayBye(sally,10);
-
-function numberDoubler(num){
-	 // console.log(num *2);
-	 const doubledValue = num *2;
-	 //return num *2
-	 return doubledValue;
-}
+// const billy = "Billy";
+// const Janderson = "Janderson";
+// const sally = "sally";
 
 
-//numberDoubler(10);
-console.log(numberDoubler(10));
+// sayBye(billy,20);
+// sayBye(Janderson,25);
+// sayBye(sally,10);
 
-const numTwoDoubled = numberDoubler(2);
-const numFiveDoubled = numberDoubler(5);
-console.log(numTwoDoubled); //4
-console.log(numFiveDoubled); //10
-console.log(numTwoDoubled + numFiveDoubled);//14
-
-
-
-
+// function numberDoubler(num){
+// 	 // console.log(num *2);
+// 	 const doubledValue = num *2;
+// 	 //return num *2
+// 	 return doubledValue;
+// }
 
 
+// //numberDoubler(10);
+// console.log(numberDoubler(10));
+
+// const numTwoDoubled = numberDoubler(2);
+// const numFiveDoubled = numberDoubler(5);
+// console.log(numTwoDoubled); //4
+// console.log(numFiveDoubled); //10
+// console.log(numTwoDoubled + numFiveDoubled);//14
+
+// function numberAdder(num1,num2){
+// 	return num1 + num2;
+// }
+
+// console.log(numberAdder(numFiveDoubled,numTwoDoubled);//14
+
+
+// const myName - "Chris";
+
+// //25:00
+
+// function sayHi(){
+// 	if (!myName) {
+// 		var myName ="Bob";
+// 	}
+// 	console.log("Hi " + MyName);
+// }
+
+//sayHi("Janderson");
+
+// var sayHi = function(myName){
+// 	if (!myName) {
+// 		var myName ="Bob";
+// 	}
+// 	console.log("Hi " + MyName);
+// }
+
+// sayHi("Janderson"); // Não funcina
+
+//29:28
 
 
 
