0982f164a67b1c9152a43317c493293871a925ac
janderson-tesla <jandersonprofissional2000@gmail.com>
2021-01-06 13:21:58 -0300

    Update part4.js
    Update

--- Aulas/part4.js
+++ Aulas/part4.js
@@ -10,7 +10,7 @@ const MyName ="Janderson";
 function sayHi(username, lastname){
 	console.log("HI" + " " + MyName + " " +lastname);
 }
-
+// não precisa tipar os parametros com C++ string firstname , int age
 function sayBye(firstname,age){
 	console.log("Bye" + " " + firstname + "you are" + " " +age);
 }
@@ -28,6 +28,26 @@ const sally = "sally";
 sayBye(billy,20);
 sayBye(Janderson,25);
 sayBye(sally,10);
+
+function numberDoubler(num){
+	 // console.log(num *2);
+	 const doubledValue = num *2;
+	 //return num *2
+	 return doubledValue;
+}
+
+
+//numberDoubler(10);
+console.log(numberDoubler(10));
+
+const numTwoDoubled = numberDoubler(2);
+const numFiveDoubled = numberDoubler(5);
+console.log(numTwoDoubled); //4
+console.log(numFiveDoubled); //10
+console.log(numTwoDoubled + numFiveDoubled);//14
+
+
+
 
 
 
